db9a932983e47cea7f5bc5d981440d5057a93287
thisisjonathan <133907284+thisisjonathan@users.noreply.github.com>
2023-12-28 22:29:02 +0100

    Move menus to seperate module

---
+++ src/editor/menu.rs
@@ -0,0 +1,152 @@
+use winit::event_loop::EventLoopProxy;
+
+use crate::{desktop::custom_event::RuffleEvent, core::Movie};
+
+use super::Editor;
+
+pub struct Menu<'a> {
+    pub name: &'a str,
+    pub items: &'a [MenuItem<'a>],
+}
+
+pub struct MenuItem<'a> {
+    pub name: &'a str,
+    pub keyboard_shortcut: Option<egui::KeyboardShortcut>,
+    pub action: fn(player: &mut Editor, event_loop: &EventLoopProxy<RuffleEvent>),
+}
+
+pub const MENUS: &[Menu] = &[
+    Menu {
+        name: "File",
+        items: &[
+            MenuItem {
+                name: "Open...",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::COMMAND,
+                    egui::Key::O,
+                )),
+                action: open_project,
+            },
+            MenuItem {
+                name: "Save",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::COMMAND,
+                    egui::Key::S,
+                )),
+                action: save_project,
+            },
+            MenuItem {
+                name: "Export",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::COMMAND,
+                    egui::Key::E,
+                )),
+                action: export_swf,
+            },
+            MenuItem {
+                name: "Close",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::COMMAND,
+                    egui::Key::W,
+                )),
+                action: close_project,
+            },
+            MenuItem {
+                name: "Exit",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::COMMAND,
+                    egui::Key::Q,
+                )),
+                action: request_exit,
+            },
+        ],
+    },
+    Menu {
+        name: "Edit",
+        items: &[
+            MenuItem {
+                name: "Undo",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::CTRL,
+                    egui::Key::Z,
+                )),
+                action: undo,
+            },
+            MenuItem {
+                name: "Redo",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::CTRL,
+                    egui::Key::R,
+                )),
+                action: redo,
+            },
+            MenuItem {
+                name: "Delete",
+                keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                    egui::Modifiers::NONE,
+                    egui::Key::Delete,
+                )),
+                action: delete_selection,
+            },
+        ],
+    },
+    Menu {
+        name: "Control",
+        items: &[MenuItem {
+            name: "Test Movie",
+            keyboard_shortcut: Some(egui::KeyboardShortcut::new(
+                egui::Modifiers::CTRL,
+                egui::Key::Enter,
+            )),
+            action: run_project,
+        }],
+    },
+    Menu {
+        name: "Help",
+        items: &[MenuItem {
+            name: "About...",
+            keyboard_shortcut: None,
+            action: show_about_screen,
+        }],
+    },
+];
+
+fn open_project(_player: &mut Editor, event_loop: &EventLoopProxy<RuffleEvent>) {
+    let _ = event_loop.send_event(RuffleEvent::OpenFile);
+}
+
+fn save_project(player: &mut Editor, _event_loop: &EventLoopProxy<RuffleEvent>) {
+    player.movie.save(&player.project_file_path);
+}
+
+fn export_swf(player: &mut Editor, _event_loop: &EventLoopProxy<RuffleEvent>) {
+    player.export_swf();
+}
+
+fn close_project(_player: &mut Editor, event_loop: &EventLoopProxy<RuffleEvent>) {
+    let _ = event_loop.send_event(RuffleEvent::CloseFile);
+}
+
+fn request_exit(_player: &mut Editor, event_loop: &EventLoopProxy<RuffleEvent>) {
+    let _ = event_loop.send_event(RuffleEvent::ExitRequested);
+}
+
+fn run_project(player: &mut Editor, _event_loop: &EventLoopProxy<RuffleEvent>) {
+    player.export_swf();
+    Movie::run(&player.directory.join("output.swf"));
+}
+
+fn show_about_screen(_player: &mut Editor, event_loop: &EventLoopProxy<RuffleEvent>) {
+    let _ = event_loop.send_event(RuffleEvent::About);
+}
+
+fn undo(player: &mut Editor, _event_loop: &EventLoopProxy<RuffleEvent>) {
+    player.do_undo();
+}
+fn redo(player: &mut Editor, _event_loop: &EventLoopProxy<RuffleEvent>) {
+    player.do_redo();
+}
+
+fn delete_selection(player: &mut Editor, _event_loop: &EventLoopProxy<RuffleEvent>) {
+    player.delete_selection();
+}
