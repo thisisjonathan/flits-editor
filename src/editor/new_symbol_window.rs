7ca5c981b24f2d26ce223be33bf4dcafbe2f1272
thisisjonathan <133907284+thisisjonathan@users.noreply.github.com>
2024-08-07 22:53:06 +0200

    Automatically focus text field in new symbol window and submit form when pressing enter

--- src/editor/new_symbol_window.rs
+++ src/editor/new_symbol_window.rs
@@ -5,6 +5,7 @@ use super::edit::{AddMovieClipEdit, MovieEdit};
 #[derive(Default)]
 pub struct NewSymbolWindow {
     name: String,
+    has_requestion_focus: bool,
 }
 impl NewSymbolWindow {
     pub fn do_ui(&mut self, egui_ctx: &egui::Context) -> Option<MovieEdit> {
@@ -15,14 +16,21 @@ impl NewSymbolWindow {
             .show(egui_ctx, |ui| {
                 egui::Grid::new("symbol_properties_grid").show(ui, |ui| {
                     ui.label("Name:");
-                    ui.add(
+                    let response = ui.add(
                         egui::TextEdit::singleline(&mut self.name).min_size(Vec2::new(200.0, 0.0)),
                     );
+                    if !self.has_requestion_focus {
+                        response.request_focus();
+                        self.has_requestion_focus = true;
+                    }
+                    let user_confirmed_form = response.lost_focus()
+                        && response.ctx.input(|i| i.key_pressed(egui::Key::Enter));
                     ui.end_row();
 
                     if ui
                         .add_enabled(!self.name.is_empty(), egui::Button::new("Create"))
                         .clicked()
+                        || (user_confirmed_form && !self.name.is_empty())
                     {
                         result = Some(MovieEdit::AddMovieClip(AddMovieClipEdit {
                             name: self.name.clone(),
