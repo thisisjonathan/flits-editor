f11bd4c43d2cc2c24358a0313bba155ea48b8dd2
thisisjonathan <133907284+thisisjonathan@users.noreply.github.com>
2024-10-12 14:14:29 +0200

    Generate third party licenses file

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cross build --target x86_64-unknown-linux-gnu --release
 cross build --target x86_64-pc-windows-gnu --release
+cargo about generate -o release/assets/shared/third-party-licenses.html about.hbs
